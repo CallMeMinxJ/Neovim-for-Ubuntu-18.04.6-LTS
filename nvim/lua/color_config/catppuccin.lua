06f008383cb2549202c2c5f00154071d4b17ad2f
astor.jiang <astor.jiang@outlook.com>
2025-12-01 00:03:31 +0800

    add startify and set fomatter

--- nvim/lua/color_config/catppuccin.lua
+++ nvim/lua/color_config/catppuccin.lua
@@ -1,6 +1,6 @@
 require("catppuccin").setup({
-  flavour = "mocha", -- latte, frappe, macchiato, mocha
-  integrations = {
-    bufferline = true, -- 启用 bufferline 集成
-  },
+    flavour = "mocha", -- latte, frappe, macchiato, mocha
+    integrations = {
+        bufferline = true, -- 启用 bufferline 集成
+    },
 })
